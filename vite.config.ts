cc060b9abc8394a403ab41102b2cdf4ddd99a12e
Autumn <aweetumn5@gmail.com>
2025-09-26 01:07:47 +0100

    Updated preview allowed hosts
    Changed the allowed hosts from
    
    Previous - zwrks.com
    new - cdn.fivemdb.net

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   preview: {
-    allowedHosts: ["zwrks.com"], 
+    allowedHosts: ["cdn.fivemdb.net"], 
   },
   plugins: [
     react(),
